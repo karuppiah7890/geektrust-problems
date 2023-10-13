14a4c401b1779a7828e5435ef806fa83b10868ef
Karuppiah Natarajan <karuppiah7890@gmail.com>
2023-10-13 12:10:44 +0530

    add tests for location methods

--- ride-sharing/src/geektrust/pkg/location_test.go
+++ ride-sharing/src/geektrust/pkg/location_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 )
 
-func LocationTest(t *testing.T) {
+func TestLocation(t *testing.T) {
 	t.Run("Get X and Y", func(t *testing.T) {
 		x := float64(1)
 		y := float64(2)
-		
+
 		location := pkg.NewLocation(x, y)
 
 		if location.GetX() != x {
@@ -19,5 +19,56 @@ func LocationTest(t *testing.T) {
 		if location.GetY() != y {
 			t.Errorf("expected getter to work but it didn't. actual: %v. expected: %v", location.GetY(), y)
 		}
+	})
+
+	t.Run("Clone", func(t *testing.T) {
+		location := pkg.NewLocation(1, 2)
+		clonedLocation := location.Clone()
+
+		if location.GetX() != clonedLocation.GetX() {
+			t.Errorf("expected x coordinates to be equal but they were not. actual: %v. expected: %v", location.GetX(), clonedLocation.GetX())
+		}
+
+		if location.GetY() != clonedLocation.GetY() {
+			t.Errorf("expected y coordinates to be equal but they were not. actual: %v. expected: %v", location.GetY(), clonedLocation.GetY())
+		}
+
+		if location == clonedLocation {
+			t.Errorf("expected cloned location to have different pointer address but got the same address")
+		}
+	})
+
+	t.Run("Equals", func(t *testing.T) {
+		t.Run("Both are equal", func(t *testing.T) {
+			x := float64(1)
+			y := float64(2)
+
+			location := pkg.NewLocation(x, y)
+			anotherLocation := pkg.NewLocation(x, y)
+
+			if !location.Equals(anotherLocation) {
+				t.Errorf("expected the two locations to be equal but they were not")
+			}
+		})
+
+		t.Run("Both are unequal", func(t *testing.T) {
+			t.Run("x is different", func(t *testing.T) {
+				location := pkg.NewLocation(5, 2)
+				anotherLocation := pkg.NewLocation(7, 2)
+
+				if location.Equals(anotherLocation) {
+					t.Errorf("expected the two locations to be unequal but they were equal")
+				}
+			})
+
+			t.Run("y is different", func(t *testing.T) {
+				location := pkg.NewLocation(1, 10)
+				anotherLocation := pkg.NewLocation(1, 30)
+
+				if location.Equals(anotherLocation) {
+					t.Errorf("expected the two locations to be unequal but they were equal")
+				}
+			})
+		})
 	})
 }
